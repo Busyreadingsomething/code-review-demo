a47c6b044e57303245e0ff1422d8c6233cac7db3
Makan Azarshahy <oro1919@gmail.com>
2018-05-31 18:45:13 -0700

    Added changes

--- client/components/AppView.jsx
+++ client/components/AppView.jsx
@@ -4,61 +4,57 @@ import axios from 'axios';
 const Puppy = props => (
   <div className="puppy">
     <h3 className="breed">{props.pup.toString().split('/')[4].toUpperCase()}</h3>
-    <a href="https://www.youtube.com/watch?v=MujRLvZ61jE"><img src={props.pup} /></a>
+    <a href="https://www.youtube.com/watch?v=MujRLvZ61jE">
+      <img src={props.pup} alt="pup" />
+    </a>
   </div>
-)
-const PuppyList = (props)=> {
-  return(
-    <div className="puppy-container">
-      {
-        props.list.map(pup => <Puppy pup={pup}/>)
-      }
-    </div>
-  )
-}
-function SearchView(props) {
-  return(
-    <div className="search-container">
-      <h3>SPELLING COUNTS!</h3>
-      <input type="text" id="search-puppy" onChange={props.search}/>
-      <input type="button" value="SEARCH" id="button" onClick={props.get}/> 
-    </div>
-  )
-}
+);
+
+const PuppyList = props => (
+  <div className="puppy-container">
+    {
+      props.list.map(pup => <Puppy pup={pup} />)
+    }
+  </div>
+);
+
+const SearchView = props => (
+  <div className="search-container">
+    <h3>SPELLING COUNTS!</h3>
+    <input type="text" id="search-puppy" onChange={props.search} />
+    <input type="button" value="SEARCH" id="button" onClick={props.get} />
+  </div>
+);
+
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       search: '',
-      cats: [1, 2, 3, 4, 5, 6],
+      puppies: [],
     };
-
-    this.handleclick = this.handleclick.bind(this);
   }
 
-  handleclick() {
+  handleClick() {
     const context = this;
     axios.get(`/puppies/${this.state.search}`)
       .then(result => context.setState({
-        cats: result.data.message
+        puppies: result.data.message,
       }));
   }
 
   handleInput(e) {
-    console.log('HIT');
     this.setState({
       search: e.target.value,
     });
-    console.log(this.state)
   }
 
   render() {
-    console.log(this.state.list)
-    return(
+    return (
       <div className="app-container">
         <h1>EVERYBODY LOVES PUPPIES!</h1>
-        <SearchView search={(e) => this.handleInput(e)} get={this.handleclick}/>
-        <PuppyList list={this.state.cats} />
+        <SearchView search={e => this.handleInput(e)} get={() => this.handleClick()}/>
+        <PuppyList list={this.state.puppies} />
       </div>
     );
   }
