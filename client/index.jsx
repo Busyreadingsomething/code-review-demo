41f46201951b6784e443c60e53b5db6d615b879d
Makan Azarshahy <oro1919@gmail.com>
2018-05-31 18:25:27 -0700

    added

--- client/index.jsx
+++ client/index.jsx
@@ -1,4 +1,7 @@
 import ReactDOM from 'react-dom';
 import App from './components/AppView';
+import helper from './helpers/index';
+
+helper();
 
 ReactDOM.render(<App />, document.getElementById('pups'));
